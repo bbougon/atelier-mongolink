4b6b23c3a84dbb1c7bf379fb99e51b577ff695e3
Bertrand Bougon <bertrand.bougon@gmail.com>
2017-11-17 01:23:06 +0100

    suppress warnings

--- src/main/java/fr/bbougon/atelier/mongo/persistence/mapping/MedicineMapping.java
+++ src/main/java/fr/bbougon/atelier/mongo/persistence/mapping/MedicineMapping.java
@@ -3,6 +3,7 @@ package fr.bbougon.atelier.mongo.persistence.mapping;
 import fr.bbougon.atelier.mongo.domain.Medicine;
 import org.mongolink.domain.mapper.AggregateMap;
 
+@SuppressWarnings("Needed for MongoLink")
 public class MedicineMapping extends AggregateMap<Medicine> {
     @Override
     public void map() {
